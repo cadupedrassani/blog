0206984c04c7f006654f104d1e4964ab557592c2
Carlos <kadu_pedrassani@hotmail.com>
2017-07-25 21:23:36 -0300

    Commit do cadastro de comentario e posts

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post, Comentario
+
+class formComentario(forms.ModelForm):
+    class Meta:
+        model = Comentario
+        fields = ('texto',)
+
+class formPost(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('texto', 'data_publicacao', 'titulo',)
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,44 @@
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Comentario
 from django.utils import timezone
+from .forms import formComentario, formPost
 
 def post_list(request):
-    posts_publicados = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
-    comentarios = Comentario.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
-    return render(request, 'post_list.html', {'posts' : posts_publicados, 'comentarios' : comentarios})
+    posts_publicados = Post.objects.order_by('data_publicacao')
+    return render(request, 'post_list.html', {'posts' : posts_publicados})
 
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
     comentarios = Comentario.objects.filter(post=post)
-    return render(request, 'post_detail.html', {'post' : post, 'comentarios': comentarios})
+    form = formComentario()
+
+    if request.method == 'POST':
+        form = formComentario(request.POST)
+        if form.is_valid():
+            comentario = form.save(commit=False)
+            comentario.autor = request.user
+            comentario.data = timezone.now()
+            comentario.post = Post.objects.get(id=pk)
+            comentario.save()
+            return redirect(post_detail, pk=pk)
+        else:
+            post = Post.objects.get(id=pk)
+            comentarios = Comentario.objects.filter(post=post).order_by('data_publicacao')
+            form = formComentario()
+
+    return render(request, 'post_detail.html', {'post' : post, 'comentarios' : comentarios, 'form' : form})
+
+def new_post(request):
+    form = formPost()
+
+    if request.method == 'POST':
+        form = formPost(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.data_criacao = timezone.now()
+            post.save()
+            return redirect(post_list)
+
+    return render(request, 'new_post.html', {'form': form})
